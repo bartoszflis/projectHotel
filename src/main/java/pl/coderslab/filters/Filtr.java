98f5c3250b544c310142c96af59fd9443cea66dc
Bartosz <bartosz.flis@gmail.com>
2018-12-26 16:43:02 +0100

    Further improvements of validation

--- src/main/java/pl/coderslab/filters/Filtr.java
+++ src/main/java/pl/coderslab/filters/Filtr.java
@@ -1,19 +1,6 @@
 package pl.coderslab.filters;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import pl.coderslab.repository.UserRepository;
-
-
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +10,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.springframework.web.servlet.config.TilesConfigurerBeanDefinitionParser.BEAN_NAME;
 
-
-@WebFilter(urlPatterns = "/jhkh")
+@WebFilter(urlPatterns = "/*")
 public class Filtr extends OncePerRequestFilter {
 
 
@@ -34,7 +19,7 @@ public class Filtr extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
-        if (isAllowedOnlyForLoggedIn(httpServletRequest.getRequestURI().toString(), httpServletRequest.getContextPath())
+        if (isAllowedOnlyForLoggedIn(httpServletRequest.getRequestURI(), httpServletRequest.getContextPath())
                 && httpServletRequest.getSession().getAttribute("email") == null) { // test if user not logged in
 
 
